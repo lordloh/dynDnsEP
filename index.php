011e66d844105e1e009a91cc4a2492f3b37afab2
Lord Bharath Bhushan Lohray <bharath@lohray.com>
2015-05-30 18:39:55 -0700

    Bug fix - hostIP.json incorrectly cerated

--- index.php
+++ index.php
@@ -79,9 +79,9 @@ function updateHostIPFile($hostName,$timestamp){
 	if ($IPDB["records"][$hostName]["ip"]!=$_SERVER["REMOTE_ADDR"]){	// if the IP Address has changed,
 		$IPDB["records"][$hostName]["ip"]=$_SERVER["REMOTE_ADDR"];		// update it.
 		$IPDB["serial"]++;
-		updateZoneFile($IPDB);								// Update the zone file;
+		updateZoneFile($IPDB);											// Update the zone file;
 	}
-	$IPDB[$hostName]["timestamp"]=time();					// update time stamp irrespective of ip change to detect stale ip addresses.
+	$IPDB["records"][$hostName]["timestamp"]=time();					// update time stamp irrespective of ip change to detect stale ip addresses.
 	$IPFile=json_encode($IPDB);
 	file_put_contents('hostIP.json', $IPFile);
 }
